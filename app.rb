c98471ba249e15012452eb6ff7537d89d854bbd3
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-05 19:46:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,6 +32,11 @@ class App < Sinatra::Base
   end 
   
   get '/:operation/:number1/:number2' do
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+    @op = params[:operation]
+    
+    "#{@num1}#{@op}#{@num2}"
     
   end 
 
